61bdca0e7a12806ee4e6a51120cb4bcc7aa8bd0b
Matteo Piras <matteo.piras@benefind.it>
2023-07-21 15:00:06 +0200

    I eat shoe box

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
+import { Box, Card } from '@mui/material'
 import React from 'react'
 
 type Props = {}
 
 const Home = (props: Props) => {
   return (
-    <div>Home</div>
+    <Box display='flex' justifyContent='center'>
+    <Card sx={{maxWidth:500}}>
+      <img src='https://i.redd.it/nt1cyljtemg31.jpg' alt='pumped up kikcs' width='100%' style={{maxWidth:'500px'}}/>
+    </Card>
+    </Box>
   )
 }
 
